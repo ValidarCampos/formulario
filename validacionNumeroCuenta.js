eb2520da3fa43271f31a68b8d1017a5cb663d358
Rivera Pérez <gregoryj733@gmail.com>
2025-07-23 18:23:17 -0400

    nuevo codigo5

--- validacionNumeroCuenta.js
+++ validacionNumeroCuenta.js
@@ -1,1 +1,10 @@
-alert("Prueba");
+window.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('input[name="element_19"]');
+  if (input) {
+    input.addEventListener('input', function () {
+      this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/^0+/, '');
+    });
+  }
+});
