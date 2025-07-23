2911c61922dc334fc78b335c71d0a3056831053f
Rivera Pérez <gregoryj733@gmail.com>
2025-07-23 18:01:21 -0400

    nuevo codigo4

--- validacionNumeroCuenta.js
+++ validacionNumeroCuenta.js
@@ -1,14 +1,1 @@
-window.addEventListener('DOMContentLoaded', function () {
-$('#element_19').on('input', function () {
-  let val = $(this).val();
-
-  // Elimina caracteres no numéricos
-  val = val.replace(/\D/g, '');
-
-  if (val.startsWith('0')) {
-    val = val.substring(1);
-  }
-
-  $(this).val(val);
-});
-});
+alert("Prueba");
